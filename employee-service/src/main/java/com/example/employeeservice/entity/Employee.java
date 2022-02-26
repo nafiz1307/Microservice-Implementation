3a403e2f0e95ca301824908a262a74a3c90c9914
Nafiz Zaman <55802844+nafiz1307@users.noreply.github.com>
2022-02-26 21:02:27 +0600

    Update Employee.java

--- employee-service/src/main/java/com/example/employeeservice/entity/Employee.java
+++ employee-service/src/main/java/com/example/employeeservice/entity/Employee.java
@@ -1,7 +1,14 @@
 package com.example.employeeservice.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collation = "employees")
+@Data
+@AllArgsConstructor
 
 public class Employee {
     @Id
